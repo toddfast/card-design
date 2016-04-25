f0725ea77cc5abe1693866ec2c501975a0bb10e2
Todd Fast <todd@digitalexistence.com>
2016-04-24 23:19:53 -0700

    Simplify directory layout; add overridable templateFile property for printable card generation

---
+++ scripts/gen-all-cards-printable-v0.1.0.jsx
@@ -0,0 +1,1 @@
+#target illustrator#include "../lib/common-print-cards.jsx"$.strict = true;(function main() {	generatePrintableCardFiles({ 		cardDirectoryName: "example-cards-v0.1.0"//		,templateFile: "templates/template-cards-bridge-cut-8in12in-v2.ai"	});})();
